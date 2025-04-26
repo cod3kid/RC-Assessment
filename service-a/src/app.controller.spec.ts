3ab4ae43acfa99461d99187983820e902f9b51bc
cod3kid <djsufail@gmail.com>
2025-04-26 14:17:52 +0400

    add test cases

--- service-a/src/app.controller.spec.ts
+++ service-a/src/app.controller.spec.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -12,11 +13,47 @@ describe('AppController', () => {
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  it('get => Should send a number to microservice and get the number multiplied back', async () => {
+    // Used a mock function because we're sending a quest to an external service.
+    jest.spyOn(appService, 'getDouble').mockResolvedValue(20);
+    const num = 10;
+    const result = await appService.getDouble(num);
+
+    expect(result).toEqual(20);
+  });
+
+  it('create => Should create a new number and return its data', async () => {
+    const requestBody = {
+      num: 23,
+    };
+    const result = await appService.createDouble(requestBody);
+
+    expect(result).toEqual({
+      message: '23 created successfully!',
+      status: 201,
+    });
+  });
+
+  it('update => should find a number by a "num" path param and update its data', async () => {
+    const num = 3;
+
+    const result = await appService.updateDouble(num);
+    expect(result).toEqual({
+      message: '3 updated successfully!',
+      status: 200,
+    });
+  });
+
+  it('delete => should find a number by a "num" path param and delete its data', async () => {
+    const num = 456;
+
+    const result = await appService.deleteDouble(num);
+    expect(result).toEqual({
+      message: '456 deleted successfully!',
+      status: 200,
     });
   });
 });
--- service-b/src/app.controller.spec.ts
+++ service-b/src/app.controller.spec.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -12,11 +13,52 @@ describe('AppController', () => {
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  it('read => Should send a number and return double the value', async () => {
+    const num = 100;
+    const result = await appService.getDouble(num);
+
+    expect(result).toEqual(200);
+  });
+
+  it('read => Should send a number and return its square', async () => {
+    const num = 5;
+    const result = await appService.getSquare(num);
+
+    expect(result).toEqual(25);
+  });
+
+  it('create => Should create a new number and return its data', async () => {
+    const requestBody = {
+      num: 23,
+    };
+    const result = await appService.createSquare(requestBody);
+
+    expect(result).toEqual({
+      message: '23 created successfully!',
+      status: 201,
+    });
+  });
+
+  it('update => should find a number by a "num" path param and update its data', async () => {
+    const num = 3;
+
+    const result = await appService.updateSquare(num);
+    expect(result).toEqual({
+      message: '3 updated successfully!',
+      status: 200,
+    });
+  });
+
+  it('delete => should find a number by a "num" path param and delete its data', async () => {
+    const num = 456;
+
+    const result = await appService.deleteSquare(num);
+    expect(result).toEqual({
+      message: '456 deleted successfully!',
+      status: 200,
     });
   });
 });
